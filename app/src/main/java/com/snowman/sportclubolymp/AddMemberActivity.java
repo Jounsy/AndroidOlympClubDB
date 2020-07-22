de981c776bd3c0e4fcf0c9eb39fa0d2ce281b756
jounsy <jouns@mail.ru>
2020-07-22 17:01:49 +0300

    Added spinner implements and menu

--- app/src/main/java/com/snowman/sportclubolymp/AddMemberActivity.java
+++ app/src/main/java/com/snowman/sportclubolymp/AddMemberActivity.java
@@ -3,9 +3,14 @@ package com.snowman.sportclubolymp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -17,6 +22,14 @@ public class AddMemberActivity extends AppCompatActivity {
     private int gender = 0;
     private ArrayAdapter spinnerAdapter;
     private ArrayList spinnerArrayList;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.edit_member_menu,menu);
+        return true;
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,5 +44,28 @@ public class AddMemberActivity extends AppCompatActivity {
         spinnerAdapter = ArrayAdapter.createFromResource(this,
                 R.array.array_gender,android.R.layout.simple_spinner_item);
         genderSpinner.setAdapter(spinnerAdapter);
+        genderSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedGender = (String)parent.getItemAtPosition(position);
+
+                if(selectedGender != null){
+
+                 if(selectedGender.equals("Male")){
+                    gender = 1;
+                }else if(selectedGender.equals("Female")){
+                    gender = 2;
+                }else {
+                     gender = 0;
+                 }
+            }
+                Toast.makeText(AddMemberActivity.this, "Пол номер = " + gender, Toast.LENGTH_SHORT).show();
+        }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                gender = 0;
+            }
+        });
     }
 }
